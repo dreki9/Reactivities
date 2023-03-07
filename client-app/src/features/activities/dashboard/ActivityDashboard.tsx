c6ff6faa8ebc2c520c633f599594c97d75d95e48
Hüseyin Emre Üğdül <drekipersonal@outlook.com>
2023-03-07 12:28:43 +0300

    end of section 7 axios configuration

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,10 +15,12 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({activities, selectedActivity, 
-    selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props) {
+    selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit,
+     deleteActivity, submitting}: Props) {
 
     return (
         <Grid>
@@ -26,6 +28,7 @@ export default function ActivityDashboard({activities, selectedActivity,
                 <ActivityList activities={activities} 
                 selectActivity={selectActivity}
                 deleteActivity={deleteActivity}
+                submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width='6'>
@@ -35,7 +38,9 @@ export default function ActivityDashboard({activities, selectedActivity,
                 openForm={openForm}
                 />}
                 {editMode &&
-                <ActivityForm activity={selectedActivity} closeForm={closeForm} createOrEdit={createOrEdit}/>}
+                <ActivityForm activity={selectedActivity} 
+                closeForm={closeForm} 
+                createOrEdit={createOrEdit} submitting={submitting}/>}
             </Grid.Column>
         </Grid>
     )
